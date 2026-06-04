02a1010d493c0cc65b85f904e02084529504d629
LiuliuLexie <99244703+LiuliuLexie@users.noreply.github.com>
2026-06-04 14:12:54 +0200

    Liuliu: qrcode and test project pages

---
+++ make_qrcode.py
@@ -0,0 +1,19 @@
+import qrcode
+import os
+
+os.makedirs("QRcode", exist_ok=True)
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+qr.add_data("https://www.berenicecourtin.com/project-b.html")
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("QRcode/qr_code.png")
+
+print("Saved: QRcode/qr_code.png")
